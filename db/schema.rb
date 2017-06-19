b772dd01be4fec296318a66897626bde93b23f58
KovaDev <ivan.kovac@klika.ba>
2017-06-19 20:58:07 +0200

    one to many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615181934) do
+ActiveRecord::Schema.define(version: 20170619181604) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20170615181934) do
     t.datetime "updated_at",  null: false
     t.text     "description"
     t.text     "about"
+    t.integer  "seller_id"
+    t.index ["seller_id"], name: "index_products_on_seller_id"
+  end
+
+  create_table "sellers", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

59caf40cf9b2582b46a03483f290dd95df2e4c85
KovaDev <ivan.kovac@klika.ba>
2017-06-29 20:52:56 +0200

    products CRUD

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :seller
+
+  validates_presence_of :name, :price
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # get '/test', controller: 'home', action: 'test'
   # get '/test' => 'home#test', as: 'nesto'
   get '/test' => 'home#test'
-  get '/products' => 'home#products'
+  # get '/products' => 'home#products'
+
+  resources :products
+  # resources :products, only: [:index, :edit, :show]
+  # resources :products, except: [:index]
 end

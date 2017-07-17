859ed7f35d5efa32bb0480eef285abfd2bfa4637
KovaDev <ivan.kovac@klika.ba>
2017-07-17 18:18:50 +0200

    refactoring of config.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
+  root "home#index"
+
+  get '/test' => 'home#test'
+
   resources :categories
   resources :sellers
   resources :users, only: [:new, :create]
-  root "home#index"
-
-  # get '/test', controller: 'home', action: 'test'
-  # get '/test' => 'home#test', as: 'nesto'
-  get '/test' => 'home#test'
-  # get '/about' => 'static_pages#about'
-  # get '/products' => 'home#products'
-
   resource :about, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :products
-  # resources :products, only: [:index, :edit, :show]
-  # resources :products, except: [:index]
 end

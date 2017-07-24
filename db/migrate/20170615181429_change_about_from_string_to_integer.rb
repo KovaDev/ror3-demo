bb3477035be7979295468cd18887bb47d5827905
KovaDev <ivan.kovac@klika.ba>
2017-07-24 19:48:06 +0200

    pg fix

--- db/migrate/20170615181429_change_about_from_string_to_integer.rb
+++ db/migrate/20170615181429_change_about_from_string_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeAboutFromStringToInteger < ActiveRecord::Migration[5.0]
   def change
-    change_column :products, :about, :integer
+    change_column :products, :about, 'integer USING CAST(column_name AS integer)'
   end
 end
